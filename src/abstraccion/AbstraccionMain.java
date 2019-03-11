11408a01f509fe3669ae0901932935eed3f4395f
Roberto Cruz Leija <robanac@gmail.com>
2019-03-11 14:25:02 -0600

    - clase abstracta

---
+++ src/abstraccion/AbstraccionMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion;
+
+/**
+ *
+ * @author Roberto Cruz Leija
+ */
+public class AbstraccionMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Burbuja b = new Burbuja();
+        BurbujaOptimizada bo = new BurbujaOptimizada();
+        // crear los arreglos 
+        // despues ordenarlos 
+    }
+    
+}
---
+++ src/abstraccion/Burbuja.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion;
+
+/**
+ *
+ * @author Roberto Cruz Leija
+ */
+public class Burbuja extends AlgoritmoOrdenamiento{
+
+    @Override
+    public void ordenar(int[] arreglo) {
+        // TO DO : ALGORITMO DE BURBUJA (asc)
+    }
+    
+}
